b7fd609fc88f291a73349a474df9b356f5617e82
out0f4mm0 <eriking2004@gmail.com>
2024-11-15 09:36:45 +0100

    Added first css font and navbar

---
+++ src/layouts/LayoutPublic/LayoutPublic.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from "./components/Navbar.jsx";
+import "../../css/header.css"
+
+const LayoutPublic = () => {
+    return (
+        <>
+            <Navbar/>
+        </>
+    );
+};
+
+export default LayoutPublic;
