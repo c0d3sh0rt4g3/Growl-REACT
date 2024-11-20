0fd883a9b5f9f34647b68ec91a097685c099878f
out0f4mm0 <eriking2004@gmail.com>
2024-11-20 10:47:13 +0100

    Added styling to form, form centering tbd

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import CustomAlert from "./components/CustomAlert.jsx";
-
+import "../../css/login-signin.css"
 const Contact = () => {
     const [contactData, setContactData] = useState({
         name: "",
@@ -83,71 +83,66 @@ const Contact = () => {
     const { name, surname, email, phone, message} = contactData
 
     return (
-        <>
-            <h2>Contact Us:</h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <input
-                        type="text"
-                        id="name"
-                        name="name"
-                        placeholder="Name"
-                        value={name}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                    />
-                    {errors.name && <p style={{color: 'red'}}>{errors.name}</p>}
-                </div>
-                <div>
-                    <input
-                        type="text"
-                        id="surname"
-                        name="surname"
-                        placeholder="Surname"
-                        value={surname}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                    />
-                    {errors.surname && <p style={{color: 'red'}}>{errors.surname}</p>}
-                </div>
-                <div>
-                    <input
-                        type="text"
-                        id="email"
-                        name="email"
-                        placeholder="Email"
-                        value={email}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                    />
-                    {errors.email && <p style={{color: 'red'}}>{errors.email}</p>}
-                </div>
-                <div>
-                    <input
-                        type="text"
-                        id="phone"
-                        name="phone"
-                        placeholder="Phone Number"
-                        value={phone}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                    />
-                    {errors.phone && <p style={{color: 'red'}}>{errors.phone}</p>}
-                </div>
-                <div>
-                    <textarea
-                        id="message"
-                        name="message"
-                        placeholder="Write us"
-                        value={message}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        rows="4"
-                        cols="50"
-                    />
-                    {errors.message && <p style={{color: 'red'}}>{errors.message}</p>}
-                </div>
-                <button type="submit">Send</button>
+        <div id={"#account-form-container"}>
+            <form id={"account-form"} onSubmit={handleSubmit}>
+                <h2>Contact Us:</h2>
+                <input
+                    type="text"
+                    className={"form-textbox"}
+                    id="name"
+                    name="name"
+                    placeholder="Name"
+                    value={name}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                />
+                {errors.name && <p style={{color: 'red'}}>{errors.name}</p>}
+                <input
+                    type="text"
+                    className={"form-textbox"}
+                    id="surname"
+                    name="surname"
+                    placeholder="Surname"
+                    value={surname}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                />
+                {errors.surname && <p style={{color: 'red'}}>{errors.surname}</p>}
+                <input
+                    type="text"
+                    className={"form-textbox"}
+                    id="email"
+                    name="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                />
+                {errors.email && <p style={{color: 'red'}}>{errors.email}</p>}
+                <input
+                    type="text"
+                    className={"form-textbox"}
+                    id="phone"
+                    name="phone"
+                    placeholder="Phone Number"
+                    value={phone}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                />
+                {errors.phone && <p style={{color: 'red'}}>{errors.phone}</p>}
+                <textarea
+                    className={"form-textbox"}
+                    id="message"
+                    name="message"
+                    placeholder="Write us"
+                    value={message}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    rows="4"
+                    cols="50"
+                />
+                {errors.message && <p style={{color: 'red'}}>{errors.message}</p>}
+                <button id={"form-login-btn"} type="submit">Send</button>
             </form>
 
             {alert.show && (
@@ -157,7 +152,7 @@ const Contact = () => {
                     onClose={() => setAlert({...alert, show: false})}
                 />
             )}
-        </>
+        </div>
     )
 }
 
