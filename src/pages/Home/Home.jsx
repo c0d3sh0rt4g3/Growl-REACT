2dfc614569dd14820605b1b360ace00c523ce547
out0f4mm0 <eriking2004@gmail.com>
2024-11-22 11:49:46 +0100

    Fixed home page css

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
 import "../../css/home.css"
 
 const Home = () => {
     return (
         <>
             <h3>Home page</h3>
+            <main id="welcoming-texts-container">
+                <h1 id="welcome-text-header">Welcome to Growl</h1>
+                <p id="welcome-text-body">Getting recipes that fit your needs have never been easier</p>
+                <NavLink to = "/search">Start searching</NavLink>
+            </main>
         </>
     );
 };
