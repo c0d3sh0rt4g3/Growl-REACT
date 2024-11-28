059752bca551779ac0d1ed9eab96bb49d709fec0
out0f4mm0 <eriking2004@gmail.com>
2024-11-28 13:42:16 +0100

    Added lazy load to searchpage

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,16 +1,17 @@
 import {createBrowserRouter} from "react-router-dom";
 import {lazy} from "react";
 import LayoutPublic from "../layouts/LayoutPublic.jsx";
-const NotFound  = lazy(() => import("../pages/NotFound/NotFound.jsx"))
 import Home from "../pages/Home/Home.jsx";
-import SearchPage from "../pages/SearchPage/SearchPage.jsx";
-const Contact = lazy(() => import("../pages/Contact/Contact.jsx"))
 import SignIn from "../pages/SignIn/SignIn.jsx";
 import SignUp from "../pages/SignUp/SignUp.jsx";
 import LayoutPrivate from "../layouts/LayoutPrivate.jsx";
+
 const Profile = lazy(() => import("../pages/Profile/Profile.jsx"))
 const EditProfile = lazy(() => import("../pages/EditProfile/EditProfile.jsx"))
 const Recipe = lazy(() => import("../pages/Recipe/Recipe.jsx"))
+const NotFound  = lazy(() => import("../pages/NotFound/NotFound.jsx"))
+const SearchPage = lazy(() => import("../pages/SearchPage/SearchPage.jsx"))
+const Contact = lazy(() => import("../pages/Contact/Contact.jsx"))
 
 
 export const router = createBrowserRouter ([
