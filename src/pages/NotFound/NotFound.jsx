73002686f2c3a4283ad521a2e128825e192b482e
c0d3sh0rt4g3 <eriking2004@gmail.com>
2024-12-08 11:24:49 +0100

    Fixed 404 page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
 import "../../css/404.css"
+import knife from "../../assets/knife.png";
 
 const NotFound = () => {
     return (
-        <>
-            <img id="rotating-knife" src="../../assets/knife.png" alt="Rotating knife"/>
+        <main id="not-found-main">
+            <img id="rotating-knife" src={knife} alt="Rotating knife"/>
             <main id="error-texts-container">
                 <h1 id="error-type">404 Page Not Found</h1>
                 <p>Sorry, but the page you were trying to view does not exist.</p>
                 <NavLink to={"/"} id={"back-to-home-btn"}> Go back to the main page</NavLink>
             </main>
-        </>
+        </main>
     );
 };
 
